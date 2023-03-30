70f721b8ca53704bf2fdb6a94d1d359423b42baf
Intel <gayaneharutyunyan2502@gmail.com>
2023-03-30 14:42:25 +0400

    Child's H&D 2

--- app/src/main/java/com/example/myapplication/FirstActivity.java
+++ app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -2,30 +2,54 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
-    private Button act_change;
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        textView = findViewById(R.id.loginNow);
 
-        act_change.setOnClickListener(
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setMovementMethod(LinkMovementMethod.getInstance());
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent();
+                        Intent intent = new Intent(".Register");
                         startActivity(intent);
                     }
                 }
         );
-            }
-        }
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(".Register");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
 
 
 
